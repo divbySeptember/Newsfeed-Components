0c7b5d6561b1f9b327ad4c899aa4c6fcb249e2ef
Anthony Johnson <lambda_school_loaner_82@Lambda-School-Loaner-82s-MacBook-Pro.local>
2019-03-06 18:05:21 -0500

    Completed MENU section

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,26 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const toggleMenu = (e) => {
+  // Toggle the "menu--open" class on your menu refence.
+  e.stopPropagation();
+  menu.classList.toggle('menu--open');
+  if (menu.classList.contains("menu--open")) {
+    TweenMax.to(menu, 1, {left: 0})
+  } else {
+    TweenMax.to(menu, 1, {left: -350})
+  };
+}
+
+const slideBack = () => {
+  if (menu.classList.contains("menu--open")) {
+    TweenMax.to(menu, 1, {left: -350})
+    menu.classList.toggle('menu--open');
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
+window.addEventListener('click', slideBack);
